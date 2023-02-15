45bb1f655fb4aba37a4e460711fc1a1eba813b8a
Sheetal <113098514+SheetalRaghmode@users.noreply.github.com>
2023-02-15 12:38:06 +0530

    Add files via upload

---
+++ Services.h
@@ -0,0 +1,16 @@
+#ifndef SERVICES_H_INCLUDED
+#define SERVICES_H_INCLUDED
+
+
+void addBookInDataBase();
+
+void searchBooks();
+
+void viewBooks();
+
+void deleteBooks();
+
+
+
+
+#endif // SERVICES_H_INCLUDED
